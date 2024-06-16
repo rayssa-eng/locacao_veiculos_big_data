56a23657b79382f2914b3c9e944153195fe507a4
Janine <mhnjqm@gmail.com>
2024-06-16 17:23:18 -0300

    Insert Categoria Veiculo

--- groups/group_m/extract_m.sql
+++ groups/group_m/extract_m.sql
@@ -3,7 +3,7 @@ SET search_path TO group_m;
 
 
 -- Insert data into dwh.Veiculo from group_m.Veiculo
-INSERT INTO dwh.Veiculo (Cd_Veiculo, Nu_Placa, Nm_Marca, Nm_Modelo, Nm_Cor, Cd_Categoria, Ds_Ar_Condicionado)
+INSERT INTO dwh.Veiculo (Cd_Veiculo, Nu_Placa, Nm_Marca, Nm_Modelo, Nm_Cor, Cd_Categoria, Ds_Ar_Condicionado, Ds_Foto)
 SELECT
     ID_Veiculo AS Cd_Veiculo,
     Placa AS Nu_Placa,
@@ -11,9 +11,19 @@ SELECT
     Modelo AS Nm_Modelo,
     Cor AS Nm_Cor,
     Grupo_Categoria AS Cd_Categoria,
-    Ar_Condicionado AS Ds_Ar_Condicionado
+    Ar_Condicionado AS Ds_Ar_Condicionado,
+    Foto_URL AS Ds_Foto
 FROM
     group_m.Veiculo;
+   
+INSERT INTO dwh.Veiculo (Cd_Categoria, Vl_Valor_da_Categoria)
+SELECT
+	ID_Grupo AS Cd_Categoria,
+	Faixa_Valor_Diaria AS Vl_Valor_da_Categoria
+
+FROM group_m.Grupo_Categoria;
+   
+   
 
 -- Insert data into dwh.Cliente from group_m.Cliente
 INSERT INTO dwh.Cliente (Cd_Cliente, Nm_Cliente, Cd_CNPJ_CPF, Endereco, Nu_Telefone, Nm_Email, Cd_CNH, Dt_Validade_CNH)
@@ -38,6 +48,7 @@ FROM
     group_m.Locacao;
 
    
+   
 INSERT INTO dwh.Reserva (Dt_Reserva, Dt_Entrega, Dt_Devolucao)
 SELECT
     Data_Reserva AS Dt_Reserva,
@@ -47,8 +58,7 @@ FROM
 	group_m.Reserva;
    
 
-  
-
+ 
 -- Insert data into dwh.Locacao from group_m.Locacao = reserva_m
 INSERT INTO dwh.Locacao (Cd_Locacao, CD_Veiculo, CD_Patio, CD_Cliente, Nu_Total_por_veiculo, Qt_Locacoes_por_veiculo)
 SELECT
